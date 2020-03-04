a783ca714f248395e9dbb4de4aefa7137da475e5
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2020-03-04 12:31:30 +0600

    CollectionView Cell

--- Shohayok/ViewController.swift
+++ Shohayok/ViewController.swift
@@ -3,12 +3,24 @@
 //  Shohayok
 //
 //  Created by Lab on 2/26/20.
-//  Copyright © 2020 WTLab. All rights reserved.
+//  Copyright © 2020 Arifeen Mahmud. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath)
+        return cell
+    }
+    
     @IBOutlet weak var SlideShowViewer: UIImageView!
      var imageNames = ["1","2","3","4","5","6"]//List of image names
     override func viewDidLoad() {
@@ -21,6 +33,9 @@ class ViewController: UIViewController {
         timer.fire() //Starts timer
             //timer.invalidate() //Stops timer
         }
+    
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    
     
 }
 
