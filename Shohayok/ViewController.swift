80670d31bc564329a77607484566c8d1c2119b43
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2020-03-05 21:59:40 +0600

    Rearrange With Extension

--- Shohayok/ViewController.swift
+++ Shohayok/ViewController.swift
@@ -2,14 +2,15 @@
 //  ViewController.swift
 //  Shohayok
 //
-//  Created by Lab on 2/26/20.
+//  Created by Arifeen Mahmud on 2/26/20.
 //  Copyright © 2020 Arifeen Mahmud. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-   
+class ViewController: UIViewController {
+    
+     @IBOutlet weak var myCollectionView: UICollectionView!
     
     @IBOutlet weak var SlideShowViewer: UIImageView!
      var imageNames = ["1","2","3","4","5"]//List of image names
@@ -18,47 +19,26 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         let timer = Timer.scheduledTimer(withTimeInterval: 1.5, repeats: true) { timer in
         self.SlideShowViewer.image = UIImage(named: self.imageNames.randomElement()!) //Slideshow logic
     }
-
-    
         timer.fire() //Starts timer
             //timer.invalidate() //Stops timer
         }
-    
-    
-    // MARK: Collection view
-    @IBOutlet weak var myCollectionView: UICollectionView!
-    
-//    let width = (view.frame.size.width - 20) / 2\
-//    let layout = collectionView(<#T##self: ViewController##ViewController#>)
-    
-//    let img = ["ec","","","","",""]
-//    
-//    let images:[UIImage] = [
-//    UIImage(named: "ec")!,
-//    UIImage(named: "")!,
-//    UIImage(named: "")!,
-//    UIImage(named: "")!,
-//    UIImage(named: "")!,
-//    UIImage(named: "")!
-//    
-//    ]
-//    
-    
-    
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-           return 2
-       }
-       func numberOfSections(in collectionView: UICollectionView) -> Int {
-           return 2
-       }
-       
-       func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-           let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath)
-           return cell
-       }
-    
-    
-}
+    }
+
+
+// MARK: Collection view
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+        
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+               return 2
+           }
+           func numberOfSections(in collectionView: UICollectionView) -> Int {
+               return 2
+           }
+           
+           func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+               let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath)
+               return cell
+           }
+        }
 
 
