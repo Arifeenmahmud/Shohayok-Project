c43b539871d63afad5789f3a1c4a2c525c60a98d
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2020-03-15 13:39:31 +0600

    Collection View Segue

--- collectionViewSegue/ViewController.swift
+++ collectionViewSegue/ViewController.swift
@@ -9,12 +9,74 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var slider: UIImageView!
+    
+    var imageNames = ["1","2","3","4","5"]//List of image names
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let timer = Timer.scheduledTimer(withTimeInterval: 1.5, repeats: true) { timer in
+            self.slider.image = UIImage(named: self.imageNames.randomElement()!) //Slideshow logic
+        }
+        
+        timer.fire() //Starts timer
+        //timer.invalidate() //Stops timer
+        
     }
-
-
+    
+    // MARK: Collection View Images
+    
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    
+    let Imgname = ["ec","em","fn","id"]
+    
+    let mainImg:[UIImage] = [
+        
+        UIImage(named: "ec")!,
+        UIImage(named: "em")!,
+        UIImage(named: "fn")!,
+        UIImage(named: "id")!
+    ]
+    
 }
+
+// MARK: Collection View
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width  = (view.frame.size.width - 40) / 2
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Imgname.count
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath) as! CollectionViewCell
+        cell.mainText.text = Imgname[indexPath.item]
+        cell.mainImageView.image = mainImg[indexPath.item]
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+           // performSegue(withIdentifier: "DetailSegue", sender: indexPath)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+            if segue.identifier == "DetailSegue"{
+            if let dest = segue.destination as? DetailViewController,
+                let index = myCollectionView.indexPathsForSelectedItems?.first {
+                dest.selection = Imgname[index.row]
+            }
+        }
+    }
+    
+}
+
 
